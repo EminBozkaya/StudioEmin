5c3eed907379f93b70d366b8423e8ae512d1de77
EminBozkaya <152wingman@gmail.com>
2023-12-23 19:14:44 +0300

    Add project files.

---
+++ PhotoEmin/Form1.Designer.cs
@@ -0,0 +1,442 @@
+﻿namespace PhotoEmin
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            btnFindFolder = new Button();
+            btnFindReceipt = new Button();
+            panel1 = new Panel();
+            panel2 = new Panel();
+            button1 = new Button();
+            lblName = new Label();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            label7 = new Label();
+            richTextBox1 = new RichTextBox();
+            label8 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            textBox4 = new TextBox();
+            textBox5 = new TextBox();
+            textBox6 = new TextBox();
+            textBox7 = new TextBox();
+            textBox8 = new TextBox();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            label9 = new Label();
+            label10 = new Label();
+            label11 = new Label();
+            label12 = new Label();
+            label14 = new Label();
+            label13 = new Label();
+            panel1.SuspendLayout();
+            panel2.SuspendLayout();
+            SuspendLayout();
+            // 
+            // btnFindFolder
+            // 
+            btnFindFolder.BackColor = Color.Transparent;
+            btnFindFolder.BackgroundImage = (Image)resources.GetObject("btnFindFolder.BackgroundImage");
+            btnFindFolder.BackgroundImageLayout = ImageLayout.Stretch;
+            btnFindFolder.FlatAppearance.BorderSize = 0;
+            btnFindFolder.FlatStyle = FlatStyle.Flat;
+            btnFindFolder.Location = new Point(37, 150);
+            btnFindFolder.Name = "btnFindFolder";
+            btnFindFolder.Size = new Size(100, 100);
+            btnFindFolder.TabIndex = 0;
+            btnFindFolder.UseVisualStyleBackColor = false;
+            // 
+            // btnFindReceipt
+            // 
+            btnFindReceipt.BackColor = Color.Transparent;
+            btnFindReceipt.BackgroundImage = (Image)resources.GetObject("btnFindReceipt.BackgroundImage");
+            btnFindReceipt.BackgroundImageLayout = ImageLayout.Stretch;
+            btnFindReceipt.FlatAppearance.BorderSize = 0;
+            btnFindReceipt.FlatStyle = FlatStyle.Flat;
+            btnFindReceipt.Location = new Point(37, 22);
+            btnFindReceipt.Name = "btnFindReceipt";
+            btnFindReceipt.Size = new Size(100, 100);
+            btnFindReceipt.TabIndex = 0;
+            btnFindReceipt.UseVisualStyleBackColor = false;
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.Transparent;
+            panel1.Controls.Add(label14);
+            panel1.Controls.Add(label12);
+            panel1.Controls.Add(label11);
+            panel1.Controls.Add(label10);
+            panel1.Controls.Add(label13);
+            panel1.Controls.Add(label9);
+            panel1.Controls.Add(button2);
+            panel1.Controls.Add(button4);
+            panel1.Controls.Add(button3);
+            panel1.Controls.Add(button1);
+            panel1.Controls.Add(panel2);
+            panel1.Location = new Point(308, 22);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(741, 686);
+            panel1.TabIndex = 1;
+            // 
+            // panel2
+            // 
+            panel2.BackgroundImage = (Image)resources.GetObject("panel2.BackgroundImage");
+            panel2.BackgroundImageLayout = ImageLayout.Stretch;
+            panel2.Controls.Add(textBox4);
+            panel2.Controls.Add(textBox7);
+            panel2.Controls.Add(textBox6);
+            panel2.Controls.Add(textBox5);
+            panel2.Controls.Add(textBox8);
+            panel2.Controls.Add(textBox3);
+            panel2.Controls.Add(textBox2);
+            panel2.Controls.Add(textBox1);
+            panel2.Controls.Add(richTextBox1);
+            panel2.Controls.Add(label5);
+            panel2.Controls.Add(label3);
+            panel2.Controls.Add(label6);
+            panel2.Controls.Add(label4);
+            panel2.Controls.Add(label7);
+            panel2.Controls.Add(label8);
+            panel2.Controls.Add(label2);
+            panel2.Controls.Add(label1);
+            panel2.Controls.Add(lblName);
+            panel2.Font = new Font("MV Boli", 11.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            panel2.ForeColor = Color.DimGray;
+            panel2.Location = new Point(26, 33);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(479, 634);
+            panel2.TabIndex = 0;
+            // 
+            // button1
+            // 
+            button1.BackgroundImage = (Image)resources.GetObject("button1.BackgroundImage");
+            button1.BackgroundImageLayout = ImageLayout.Stretch;
+            button1.FlatAppearance.BorderSize = 0;
+            button1.FlatStyle = FlatStyle.Flat;
+            button1.Location = new Point(583, 451);
+            button1.Name = "button1";
+            button1.Size = new Size(80, 80);
+            button1.TabIndex = 1;
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // lblName
+            // 
+            lblName.Location = new Point(94, 79);
+            lblName.Name = "lblName";
+            lblName.Size = new Size(100, 23);
+            lblName.TabIndex = 0;
+            lblName.Text = "Ad:";
+            // 
+            // label1
+            // 
+            label1.Location = new Point(94, 123);
+            label1.Name = "label1";
+            label1.Size = new Size(100, 23);
+            label1.TabIndex = 0;
+            label1.Text = "Soyad:";
+            // 
+            // label2
+            // 
+            label2.Location = new Point(199, 224);
+            label2.Name = "label2";
+            label2.Size = new Size(76, 23);
+            label2.TabIndex = 0;
+            label2.Text = "Alınan:";
+            // 
+            // label3
+            // 
+            label3.Location = new Point(284, 224);
+            label3.Name = "label3";
+            label3.Size = new Size(77, 23);
+            label3.TabIndex = 0;
+            label3.Text = "Kalan:";
+            // 
+            // label4
+            // 
+            label4.Location = new Point(94, 172);
+            label4.Name = "label4";
+            label4.Size = new Size(100, 23);
+            label4.TabIndex = 0;
+            label4.Text = "Ebat:";
+            // 
+            // label5
+            // 
+            label5.Location = new Point(250, 172);
+            label5.Name = "label5";
+            label5.Size = new Size(56, 23);
+            label5.TabIndex = 0;
+            label5.Text = "Adet:";
+            // 
+            // label6
+            // 
+            label6.Location = new Point(114, 224);
+            label6.Name = "label6";
+            label6.Size = new Size(64, 23);
+            label6.TabIndex = 0;
+            label6.Text = "Tutar:";
+            // 
+            // label7
+            // 
+            label7.Location = new Point(153, 309);
+            label7.Name = "label7";
+            label7.Size = new Size(126, 23);
+            label7.TabIndex = 0;
+            label7.Text = "Teslim Tarihi:";
+            // 
+            // richTextBox1
+            // 
+            richTextBox1.Location = new Point(94, 370);
+            richTextBox1.Name = "richTextBox1";
+            richTextBox1.Size = new Size(294, 219);
+            richTextBox1.TabIndex = 1;
+            richTextBox1.Text = "";
+            // 
+            // label8
+            // 
+            label8.Location = new Point(94, 344);
+            label8.Name = "label8";
+            label8.Size = new Size(100, 23);
+            label8.TabIndex = 0;
+            label8.Text = "Not:";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(153, 76);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(235, 32);
+            textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(153, 120);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(235, 32);
+            textBox2.TabIndex = 2;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(306, 169);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(82, 32);
+            textBox3.TabIndex = 3;
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(272, 306);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(116, 32);
+            textBox4.TabIndex = 3;
+            // 
+            // textBox5
+            // 
+            textBox5.Location = new Point(114, 250);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(79, 32);
+            textBox5.TabIndex = 3;
+            // 
+            // textBox6
+            // 
+            textBox6.Location = new Point(199, 250);
+            textBox6.Name = "textBox6";
+            textBox6.Size = new Size(79, 32);
+            textBox6.TabIndex = 3;
+            // 
+            // textBox7
+            // 
+            textBox7.Location = new Point(284, 250);
+            textBox7.Name = "textBox7";
+            textBox7.Size = new Size(77, 32);
+            textBox7.TabIndex = 3;
+            // 
+            // textBox8
+            // 
+            textBox8.Location = new Point(153, 169);
+            textBox8.Name = "textBox8";
+            textBox8.Size = new Size(82, 32);
+            textBox8.TabIndex = 3;
+            // 
+            // button2
+            // 
+            button2.BackgroundImage = (Image)resources.GetObject("button2.BackgroundImage");
+            button2.BackgroundImageLayout = ImageLayout.Stretch;
+            button2.FlatAppearance.BorderSize = 0;
+            button2.FlatStyle = FlatStyle.Flat;
+            button2.Location = new Point(583, 553);
+            button2.Name = "button2";
+            button2.Size = new Size(80, 80);
+            button2.TabIndex = 1;
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.BackgroundImage = (Image)resources.GetObject("button3.BackgroundImage");
+            button3.BackgroundImageLayout = ImageLayout.Stretch;
+            button3.FlatAppearance.BorderSize = 0;
+            button3.FlatStyle = FlatStyle.Flat;
+            button3.Location = new Point(511, 257);
+            button3.Name = "button3";
+            button3.Size = new Size(214, 168);
+            button3.TabIndex = 1;
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            button4.BackgroundImage = (Image)resources.GetObject("button4.BackgroundImage");
+            button4.BackgroundImageLayout = ImageLayout.Stretch;
+            button4.FlatAppearance.BorderSize = 0;
+            button4.FlatStyle = FlatStyle.Flat;
+            button4.Location = new Point(549, 78);
+            button4.Name = "button4";
+            button4.Size = new Size(142, 116);
+            button4.TabIndex = 1;
+            button4.UseVisualStyleBackColor = true;
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Font = new Font("Palatino Linotype", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label9.Location = new Point(574, 64);
+            label9.Name = "label9";
+            label9.Size = new Size(99, 22);
+            label9.TabIndex = 2;
+            label9.Text = "Konum Seç:";
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.FlatStyle = FlatStyle.Flat;
+            label10.Font = new Font("Palatino Linotype", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label10.Location = new Point(555, 409);
+            label10.Name = "label10";
+            label10.Size = new Size(136, 22);
+            label10.TabIndex = 2;
+            label10.Text = "Kaydet ve Yazdır";
+            // 
+            // label11
+            // 
+            label11.AutoSize = true;
+            label11.FlatStyle = FlatStyle.Flat;
+            label11.Font = new Font("Palatino Linotype", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label11.Location = new Point(592, 530);
+            label11.Name = "label11";
+            label11.Size = new Size(63, 22);
+            label11.TabIndex = 2;
+            label11.Text = "Kaydet";
+            // 
+            // label12
+            // 
+            label12.AutoSize = true;
+            label12.FlatStyle = FlatStyle.Flat;
+            label12.Font = new Font("Palatino Linotype", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label12.Location = new Point(593, 625);
+            label12.Name = "label12";
+            label12.Size = new Size(58, 22);
+            label12.TabIndex = 2;
+            label12.Text = "Yazdır";
+            // 
+            // label14
+            // 
+            label14.BackColor = Color.WhiteSmoke;
+            label14.BorderStyle = BorderStyle.Fixed3D;
+            label14.Location = new Point(622, 197);
+            label14.Name = "label14";
+            label14.Size = new Size(100, 23);
+            label14.TabIndex = 4;
+            // 
+            // label13
+            // 
+            label13.AutoSize = true;
+            label13.Font = new Font("Palatino Linotype", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label13.Location = new Point(511, 198);
+            label13.Name = "label13";
+            label13.Size = new Size(105, 18);
+            label13.TabIndex = 2;
+            label13.Text = "Seçilen Konum:";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackgroundImage = (Image)resources.GetObject("$this.BackgroundImage");
+            BackgroundImageLayout = ImageLayout.Stretch;
+            ClientSize = new Size(1528, 742);
+            Controls.Add(panel1);
+            Controls.Add(btnFindFolder);
+            Controls.Add(btnFindReceipt);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            panel2.ResumeLayout(false);
+            panel2.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+        private Button btnFindFolder;
+        private Button btnFindReceipt;
+        private Panel panel1;
+        private Button button1;
+        private Panel panel2;
+        private Label lblName;
+        private Label label5;
+        private Label label3;
+        private Label label6;
+        private Label label4;
+        private Label label7;
+        private Label label2;
+        private Label label1;
+        private TextBox textBox4;
+        private TextBox textBox3;
+        private TextBox textBox2;
+        private TextBox textBox1;
+        private RichTextBox richTextBox1;
+        private Label label8;
+        private TextBox textBox7;
+        private TextBox textBox6;
+        private TextBox textBox5;
+        private TextBox textBox8;
+        private Button button2;
+        private Button button4;
+        private Button button3;
+        private Label label10;
+        private Label label9;
+        private Label label12;
+        private Label label11;
+        private Label label14;
+        private Label label13;
+    }
+}
